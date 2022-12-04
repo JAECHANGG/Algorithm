5adb15b1e2e9eaf98ea5088c2649cd173556c027
JAECHANGG <108451940+JAECHANGG@users.noreply.github.com>
2022-12-04 14:10:34 +0900

    [level 0] Title: 순서쌍의 개수, Time: 4.97 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -1,9 +1,10 @@
 function solution(n) {
-    const result = []
-    // 1부터 n까지 반복하며 모든 약수를 정답 배열에 입력
-    for(let i = 1 ; i <= n ; i ++) {
-        if(n%i === 0) result.push(i)
+ 
+    let cnt = 0;
+    for(let i = 1; i<=n; i++){
+    if (n % i === 0) {
+        cnt++;
+        }
     }
-    // 모든 약수의 수는 순서쌍의 수와 같음
-    return result.length
+    return cnt;
 }
