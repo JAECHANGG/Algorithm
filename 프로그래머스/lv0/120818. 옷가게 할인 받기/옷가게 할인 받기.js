fb0d97bce765bf5fcd3094e4bcfe2684404f8f7d
JAECHANGG <108451940+JAECHANGG@users.noreply.github.com>
2022-12-04 13:26:33 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 33.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -1,13 +1,7 @@
 function solution(price) {
+    var answer = 0;
     if (price < 100000) return price
-    else if (price >= 100000 && price < 300000) {
-        return Math.floor(price - price * 0.05)
-    } else if (price >= 300000 && price < 500000) {
-        return Math.floor(price - price * 0.1)
-    } else if (price >= 500000) {
-        return Math.floor(price - price * 0.2)
-    }
+    else if(price >= 100000 && price < 300000) return Math.floor(price * 0.95)
+    else if(price >= 300000 && price < 500000) return Math.floor(price * 0.9)
+    else return Math.floor(price*0.8)
 }
-
-// if문 활용, 순서대로 코드가 실행되므로 논리 연산자를 써야함
-// 조건에 맞게 price(원래 가격) - price * %(할인 가격) 해줘서 리턴
