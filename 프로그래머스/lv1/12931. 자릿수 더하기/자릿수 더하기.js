3ee656dc377793332f7f306ade9580c455142ca6
JAECHANGG <108451940+JAECHANGG@users.noreply.github.com>
2022-12-06 09:13:03 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -1,12 +1,4 @@
 function solution(n)
 {
-    var answer = 0;
-    n = n.toString();
-    console.log(n)
-    for (let i=0; i<n.length; i++){
-        answer += parseInt(n[i])
-        console.log(n[i])
-    }
-
-    return answer;
+    return n.toString().split("").reduce((acc, cur) => acc + Number(cur), 0)
 }
