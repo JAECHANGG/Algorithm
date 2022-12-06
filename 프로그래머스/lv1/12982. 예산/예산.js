44492d1a885349e12314fd174703970f8c5c2db6
JAECHANGG <108451940+JAECHANGG@users.noreply.github.com>
2022-12-06 10:16:45 +0900

    [level 1] Title: 예산, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -0,0 +1,10 @@
+function solution(d, budget) {
+    let temp = 0
+    let answer = 0
+    let sor = d.sort((a,b) => a-b)
+    while(budget >= temp){
+          temp += sor[answer]
+        answer++
+          }
+    return answer - 1
+}
