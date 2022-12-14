6ae9593651cc4c32086bda9e90512c569ec78d42
JAECHANGG <108451940+JAECHANGG@users.noreply.github.com>
2022-12-15 08:50:42 +0900

    [level 1] Title: 약수의 합, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
@@ -1,9 +1,7 @@
 function solution(n) {
-    let num = 0;
-    for(let i = 1; i<=n; i++) {
-        console.log(i)
-       if (n % i === 0)
-        num += i
+    let answer = 0;
+    for (let i = 0; i<=n; i++) {
+        if (n % i === 0 ) answer += i
     }
-    return num
+    return answer
 }
