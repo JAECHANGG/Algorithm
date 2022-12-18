0909b00ea844020998985bf3214450b1c7ca20a6
JAECHANGG <108451940+JAECHANGG@users.noreply.github.com>
2022-12-18 18:51:38 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -1,7 +1,3 @@
 function solution(seoul) {
-    let answer = 0
-    for(let i=0; i<seoul.length; i++) {
-        if(seoul[i] === "Kim") answer += i
-    }
-    return `김서방은 ${answer}에 있다`
+    return `김서방은 ${seoul.indexOf("Kim")}에 있다`
 }
