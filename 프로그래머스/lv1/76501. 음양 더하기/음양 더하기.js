611e232e657b95e02157585fb4d6aa6f28799867
JAECHANGG <108451940+JAECHANGG@users.noreply.github.com>
2022-12-18 19:46:05 +0900

    [level 1] Title: 음양 더하기, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
@@ -1,3 +1,3 @@
 function solution(absolutes, signs) {
-    return absolutes.reduce((acc, cur, index) => acc += signs[index] ? cur*1: cur*-1, 0)
+    return absolutes.reduce((acc, cur, idx) => acc + (cur * (signs[idx] ? 1 : -1)), 0)
 }
