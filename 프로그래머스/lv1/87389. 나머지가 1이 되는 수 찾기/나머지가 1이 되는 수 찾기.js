1f12b2bdb766e290c087f6cf9c2b5ab602de5579
JAECHANGG <108451940+JAECHANGG@users.noreply.github.com>
2022-12-18 18:32:17 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.js
+++ 프로그래머스/lv1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.js
@@ -1,8 +1,7 @@
 function solution(n) {
-    for (let i = 1; i<n; i++) {
-       if(n % i === 1) return i
+    for (let i = 1; i<n; i++){
+        if( n % i === 1 ) return i  
     }
 }
 
-// n % x === 1 인 가장 작은 수 찾기
-// 바로 reutrn 하면 됨
+// n % x === 1 인 수 중 가장 작은 수
