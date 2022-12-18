3924187b6e626f10f3b247bff8df3a51c2cb2985
JAECHANGG <108451940+JAECHANGG@users.noreply.github.com>
2022-12-18 19:39:34 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,13 +1,6 @@
 function solution(arr) {
-   let min = Math.min(...arr)
-   if (arr.length <= 1) return [-1]
-   else return arr.filter(el => el !== min)
+    if (arr.length === 1) return [-1]
+    
+    let min = Math.min(...arr)
+    return arr.filter(el=> el !== min)
 }
-
-// 첫번째 방법 ( 틀림 )
-// sort로 정렬하고 0번째 인덱스 제거
-// 만약 10이라면 -1 리턴!
-
-// 두번째 방법
-// filter로 Math.min()써서 거른다.
-// 10일때면 -1 리턴!
