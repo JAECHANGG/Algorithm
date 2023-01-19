5f2d9ceecbfdcfe59dfb492034176e6e3e230462
JAECHANGG <108451940+JAECHANGG@users.noreply.github.com>
2023-01-19 10:10:35 +0900

    [level 0] Title: 영어가 싫어요, Time: 2.35 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -2,9 +2,10 @@ function solution(numbers) {
     const num = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
     
     for(let i = 0; i < num.length; i++){
-  
+  // 만약 split("one") 라면 [ '', 'twothreefourfivesixseveneightnine' ]
+  // .join(i), i=1 라면, 나뉘어진 부분에 1이 들어가서 문자열로 합쳐진다. '1twothreefourfivesixseveneightnine' 
         numbers = numbers.split(num[i]).join(i)
-
+console.log(numbers)
     }
     
     return Number(numbers)
