24519d39bcd88995e6f310083f45cecbbf37fe3a
JAECHANGG <108451940+JAECHANGG@users.noreply.github.com>
2023-04-04 10:28:32 +0900

    [level 1] Title: K번째수, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.js
+++ 프로그래머스/lv1/42748. K번째수/K번째수.js
@@ -1,9 +1,7 @@
 function solution(array, commands) {
     let answer = []
     for(let a = 0; a<commands.length; a++) {
-        const i = commands[a][0]
-        const j = commands[a][1]
-        const k = commands[a][2]
+        const [i, j, k] = commands[a]
         answer.push(array.slice(i-1, j).sort((a,b) => a-b)[k-1])
     }
     return answer
