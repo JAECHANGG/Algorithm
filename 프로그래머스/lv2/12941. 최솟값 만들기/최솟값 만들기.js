c5026c9ad2e9c0c787194b9b46fb84adbddea1e6
JAECHANGG <108451940+JAECHANGG@users.noreply.github.com>
2023-04-05 10:36:00 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.75 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -1,9 +1,5 @@
-function solution(a,b){
-    var answer = 0;
-    const res1 = a.sort((a,b)=> a-b)
-    const res2 = b.sort((a,b)=> b-a)
-    for(i=0;i<res1.length;i++){
-      answer += res1[i] * res2[i]
-    }
-    return answer;
+function solution(A,B){
+     A.sort((a, b) => a - b)
+    B.sort((a, b) => b - a)
+    return A.reduce((total, val, idx) => total + val * B[idx], 0)
 }
