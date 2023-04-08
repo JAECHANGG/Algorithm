203b7e505585918a08c5141f981492470f1032fa
JAECHANGG <108451940+JAECHANGG@users.noreply.github.com>
2023-04-08 14:01:12 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
+++ 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
@@ -1,13 +1,11 @@
 function solution(s) {
-    let ans = [];
-    
-    let sArr = s.split("");
-    
-    sArr.map((item) => {
-        if(s.indexOf(item) === s.lastIndexOf(item)){
-            ans.push(item);
+
+    const a = s.split("").sort().join("")
+    var b = ""
+    for(let i = 0;i<a.length;i++){
+        if(a[i] != a[i-1] && a[i] != a[i+1]){
+            b += a[i]
         }
-    })
-    
-    return ans.sort().join("");
+    }
+    return b;
 }
