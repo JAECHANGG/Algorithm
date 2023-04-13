a7ded77af3ca9e0900c69a91f2c53939c2889dae
JAECHANGG <108451940+JAECHANGG@users.noreply.github.com>
2023-04-14 04:04:15 +0900

    [level 1] Title: 2016년, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.js
+++ 프로그래머스/lv1/12901. 2016년/2016년.js
@@ -1,4 +1,10 @@
 function solution(a, b) {
-    const week = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
-    return week[new Date(`2016-${a}-${b}`).getDay()]
+    const monthDay = [31,29,31,30,31,30,31,31,30,31,30,31]
+    const weekDay = ["THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"]
+
+    let days = b
+    for(let i=0 ; i<a-1 ; i++)
+        days += monthDay[i]
+
+    return weekDay[days%7];
 }
