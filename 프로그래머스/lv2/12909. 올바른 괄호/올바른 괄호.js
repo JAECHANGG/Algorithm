6c91ba355a76c6ba6696081acf0d9702b153c3b7
JAECHANGG <108451940+JAECHANGG@users.noreply.github.com>
2023-04-25 01:41:28 +0900

    [level 2] Title: 올바른 괄호, Time: 2.59 ms, Memory: 36.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,17 @@
+function solution(s){
+    if(s[0] === ')' || s[s.length-1] === '(') {
+        return false
+    }
+    let count = 0;
+    for(let i=0; i<s.length; i++) {
+        if(s[i] === '(') {
+            count++;
+        } else {
+            count--;
+        }
+        if(count < 0) {
+                return false;
+            }
+    }
+    return count === 0 ? true : false
+}
