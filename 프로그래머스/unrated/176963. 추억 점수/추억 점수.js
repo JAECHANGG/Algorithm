8aeaec6b413a53c22f8871d12f7fec5c0bc6bcef
JAECHANGG <108451940+JAECHANGG@users.noreply.github.com>
2023-04-24 20:04:54 +0900

    [unrated] Title: 추억 점수, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.js
@@ -0,0 +1,5 @@
+function solution(name, yearning, photo) {
+    return photo.map(v => {
+      return v.filter(x => x = name.includes(x)).map(v => yearning[name.indexOf(v)]).reduce((acc, cur) => acc + cur,0)
+    });
+}
