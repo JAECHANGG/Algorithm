b12fdf33fa5f6b1ed101a53b7cfd495b6f4425a4
JAECHANGG <108451940+JAECHANGG@users.noreply.github.com>
2023-06-13 03:11:56 +0900

    [level 2] Title: 구명보트, Time: 3.62 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/README.md
+++ 프로그래머스/lv2/42885. 구명보트/README.md
@@ -1,10 +1,10 @@
 # [level 2] 구명보트 - 42885 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885#) 
 
 ### 성능 요약
 
-메모리: 38.9 MB, 시간: 12.26 ms
+메모리: 38.1 MB, 시간: 3.62 ms
 
 ### 구분
 
--- 프로그래머스/lv2/42885. 구명보트/구명보트.js
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.js
@@ -1,17 +1,8 @@
 function solution(people, limit) {
     var answer = 0;
-    people.sort((a, b) => a-b)
     const human = people.filter(num => num <= limit-40);
     const len = people.filter(num => num > limit-40).length;
-    
-//     if(human.length === 0) {
-//         return len;
-//     }
-    
-//     if(human.length === 1) {
-//         return 1 + len;
-//     }
-    // [50,50,50,50,60,60,60,60,70,70,80,80,90,90] 12
+    human.sort((a, b) => a-b)
     
     for(let i=0; i<human.length; i++) {
         for(let j=human.length-1; j>i; j--) {
@@ -19,15 +10,7 @@ function solution(people, limit) {
                     human.splice(j, 1)
                     break;
             }
-            
-            // if(i === j) break;
-            
-            // if(i === j) {
-            //     answer++;
-            // }
-            // answer++;
         }
-        // console.log(human)
     }
     answer += human.length;
     return answer + len;
