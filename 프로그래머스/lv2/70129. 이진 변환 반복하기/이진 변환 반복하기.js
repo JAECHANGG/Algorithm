42915e76a95552354608c5c80c297e32f1f29b76
JAECHANGG <108451940+JAECHANGG@users.noreply.github.com>
2023-06-15 12:19:51 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 9.12 ms, Memory: 39.1 MB -BaekjoonHub

--- 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -1,7 +1,7 @@
 function solution(s) {
     let count1 = 0;
     let count2 = 0;
-    let c = 0;
+    
     while(s.length >= 1) {
          const str = s.split('').filter(num => num === "0").length;
          count1 += 1;
