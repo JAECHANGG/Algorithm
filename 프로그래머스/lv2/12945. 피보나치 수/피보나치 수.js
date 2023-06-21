c28f13f6abcee3dd9688456ade1fe4748d344a5a
JAECHANGG <108451940+JAECHANGG@users.noreply.github.com>
2023-06-21 10:50:43 +0900

    [level 2] Title: 피보나치 수, Time: 1.55 ms, Memory: 36.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    var answer = 0;
+    let num1 = 0;
+    let num2 = 1;
+    for(let i=2; i<=n; i++) {
+        answer = (num1 + num2) % 1234567;
+        num1 = num2;
+        num2 = answer;
+    }
+    return answer;
+}
