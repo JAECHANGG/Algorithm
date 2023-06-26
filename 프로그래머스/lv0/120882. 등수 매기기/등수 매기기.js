abb2763a1e904d7e3458cc41f2bcd24fd6486f9e
JAECHANGG <108451940+JAECHANGG@users.noreply.github.com>
2023-06-26 12:35:01 +0900

    [level 0] Title: 등수 매기기, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.js
@@ -0,0 +1,15 @@
+function solution(score) {
+    const answer = [];
+    const average = score.map((arr) => (arr[0] + arr[1]) / 2);
+    const sorted = [...average].sort((a, b) => b - a);
+
+    for(let i=0; i<average.length; i++) {
+        for(let j=0; j<sorted.length; j++) {
+            if(average[i] === sorted[j]) {
+                answer.push(j+1);
+                break;
+            }
+        }
+    }
+    return answer;
+}
