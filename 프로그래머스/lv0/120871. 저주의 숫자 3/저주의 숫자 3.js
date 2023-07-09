694633ea4f919838e12f95e30293dd128c3849b6
JAECHANGG <108451940+JAECHANGG@users.noreply.github.com>
2023-07-09 12:31:08 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.31 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
@@ -0,0 +1,64 @@
+function solution(n) {
+    var answer = 1;
+    for(let i=2; i<n+1; i++) {
+        answer++;
+        if(answer % 3 === 0 || String(answer).includes('3')) {
+            answer++;
+        }
+        if(answer % 3 === 0 || String(answer).includes('3')) {
+            answer++;
+        }
+        if(answer % 3 === 0 || String(answer).includes('3')) {
+            answer++;
+        }
+        if(answer % 3 === 0 || String(answer).includes('3')) {
+            answer++;
+        }
+        if(answer % 3 === 0 || String(answer).includes('3')) {
+            answer++;
+        }
+        if(answer % 3 === 0 || String(answer).includes('3')) {
+            answer++;
+        }
+        if(answer % 3 === 0 || String(answer).includes('3')) {
+            answer++;
+        }
+        if(answer % 3 === 0 || String(answer).includes('3')) {
+            answer++;
+        }
+        if(answer % 3 === 0 || String(answer).includes('3')) {
+            answer++;
+        }
+        if(answer % 3 === 0 || String(answer).includes('3')) {
+            answer++;
+        }
+        if(answer % 3 === 0 || String(answer).includes('3')) {
+            answer++;
+        }
+        if(answer % 3 === 0 || String(answer).includes('3')) {
+            answer++;
+        }
+        if(answer % 3 === 0 || String(answer).includes('3')) {
+            answer++;
+        }
+        if(answer % 3 === 0 || String(answer).includes('3')) {
+            answer++;
+        }
+        if(answer % 3 === 0 || String(answer).includes('3')) {
+            answer++;
+        }
+        if(answer % 3 === 0 || String(answer).includes('3')) {
+            answer++;
+        }
+        if(answer % 3 === 0 || String(answer).includes('3')) {
+            answer++;
+        }
+        if(answer % 3 === 0 || String(answer).includes('3')) {
+            answer++;
+        }
+        if(answer % 3 === 0 || String(answer).includes('3')) {
+            answer++;
+        }
+    }
+    return answer;
+}
