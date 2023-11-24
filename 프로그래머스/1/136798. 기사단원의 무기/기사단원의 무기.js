b3943951dc919d76eb199aa5d109ce7ae281123b
JAECHANGG <108451940+JAECHANGG@users.noreply.github.com>
2023-11-24 16:42:44 +0900

    [level 1] Title: 기사단원의 무기, Time: 2.66 ms, Memory: 36.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.js
@@ -0,0 +1,16 @@
+function solution(number, limit, power) {
+    var answer = 0;
+    let count = 0;
+    
+    for(let i=1; i<=number; i++) {
+        for(let j=1; j<=Math.sqrt(i); j++) {
+            if(i % j === 0) {
+                count++
+                if(i / j !== j) count++
+            }
+        }
+        answer += count  > limit ? power : count 
+        count = 0;
+    }
+    return answer;
+}
